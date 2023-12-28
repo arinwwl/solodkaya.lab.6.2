e0547e354b8e242012308805705294d4faa20670
Arina <isp_2_1@mail.ru>
2023-12-28 12:22:58 +0200

    Добавьте файлы проекта.

---
+++ solodkaya.lab.6.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Numerics;     
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 4;   
+        int m = 8;   
+
+       
+        int[,] matrix = new int[n, m];
+        Random random = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                matrix[i, j] = random.Next(-100, 100);
+            }
+        }
+        int count = 0;            
+        BigInteger product = 1;   
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[i, j] > 0)
+                {
+                    count++;
+                    product *= matrix[i, j];
+                }
+            }
+        }
+
+        double averageGeom = Math.Pow((double)product, 1.0 / count);
+
+        Console.WriteLine("Среднее геометрическое положительных элементов матрицы: {0}", averageGeom);
+    }
+}
